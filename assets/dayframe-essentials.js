dbbaffa8a2306af9b32f0bea02e33c1d19b57e14
Celine <120723839+celinexal@users.noreply.github.com>
2026-08-31 08:09:31 +0100

    Bump Essentials design asset version

--- assets/dayframe-essentials.js
+++ assets/dayframe-essentials.js
@@ -4,7 +4,7 @@
   const STYLE_ID = 'df-essentials-loader-style';
   const CLEANUP_SRC = '/assets/dayframe-essentials-cleanup.js?v=20260831-essentials-cleanup-v3';
   const FLO_SRC = '/assets/dayframe-essentials-flo.js?v=20260831-flo-v1';
-  const MORE_SRC = '/assets/dayframe-essentials-more.js?v=20260831-more-v3';
+  const MORE_SRC = '/assets/dayframe-essentials-more.js?v=20260831-more-design-v4';
   const CLICKFIX_SRC = '/assets/dayframe-essentials-clickfix.js?v=20260831-clickfix-v4';
 
   if (!document.getElementById(STYLE_ID)) {
