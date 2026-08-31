62824f28ad5810770cf3530b4abe26a547ca4d8b
Celine <120723839+celinexal@users.noreply.github.com>
2026-08-31 11:55:19 +0100

    Load MyFlo clickfix v9

--- assets/dayframe-essentials.js
+++ assets/dayframe-essentials.js
@@ -5,7 +5,7 @@
   const CLEANUP_SRC = '/assets/dayframe-essentials-cleanup.js?v=20260831-essentials-cleanup-v3';
   const FLO_SRC = '/assets/dayframe-essentials-flo.js?v=20260831-myflo-v2';
   const MORE_SRC = '/assets/dayframe-essentials-more.js?v=20260831-more-design-v6';
-  const CLICKFIX_SRC = '/assets/dayframe-essentials-clickfix.js?v=20260831-clickfix-v8';
+  const CLICKFIX_SRC = '/assets/dayframe-essentials-clickfix.js?v=20260831-clickfix-v9';
   const HOME_DESC = 'My Car, MyFlo, documents and reminders in one place.';
   const MOBILE_DESC = 'My Car, MyFlo, documents and reminders';
 
